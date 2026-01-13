9e95c3f82a78211b83123b2aa765dcf1d7ccca3f
Monis Azeem <monis@Moniss-MacBook-Air.local>
2026-01-13 18:34:58 +0530

    ui created along with merkle tree changes

--- merkle-tree/merkle-tree.ts
+++ merkle-tree/merkle-tree.ts
@@ -1,8 +1,9 @@
 import {ethers, keccak256} from 'ethers'
+import { Buffer } from 'buffer'
 
 type hexString = `0x${string}`
 
-function calculateAddressHash(address: hexString): hexString{
+export function calculateAddressHash(address: hexString): hexString{
     const paddedAddress = ethers.zeroPadValue(address, 32) //solidity never hash 20 bytes which addresses are usually of, it hashes 32 bytes which is standard word size in Ethereum
     const firstHash = keccak256(paddedAddress)
     const secondHash = keccak256(firstHash) //so leafs are usually hashed twice and internal node once. This is done to prevent second preimage attack added by OpenZepplin as a standard safety measure.
@@ -18,7 +19,7 @@ function calculatePairHash(hash1: hexString, hash2: hexString): hexString{
     
 }
 
-function calculateNewLevel(tempArr: Array<`0x${string}`>){ //TODO: add return type here
+function calculateNewLevel(tempArr: Array<`0x${string}`>){
     let arr = []
 
     for(let left=0; left<tempArr.length; left+=2){
@@ -35,7 +36,7 @@ function calculateNewLevel(tempArr: Array<`0x${string}`>){ //TODO: add return ty
     return arr
 }
 
-function createTree(arr: Array<`0x${string}`>): Array<Array<`0x${string}`>>{
+export function createTree(arr: Array<`0x${string}`>): Array<Array<`0x${string}`>>{
     if(arr.length === 0)
         return [[]]
 
@@ -62,8 +63,7 @@ function createTree(arr: Array<`0x${string}`>): Array<Array<`0x${string}`>>{
 }
 
 //proof : Array of sibling hashes to prove that leaf belongs to the tree.
-//TODO: Check for condition when there are one or two leafs.
-function generateProof(tree: Array<Array<`0x${string}`>>, hashIndex: number): Array<`0x${string}`> {
+export function generateProof(tree: Array<Array<`0x${string}`>>, hashIndex: number): Array<`0x${string}`> {
     let proofArr = []
     if(hashIndex === 0 && tree.length === 1){
         //@ts-ignore
@@ -89,11 +89,15 @@ function generateProof(tree: Array<Array<`0x${string}`>>, hashIndex: number): Ar
 
     }
 
-    return proofArr as Array<`0x${string}`>  
+    return proofArr.filter((item) => item !== undefined) as Array<`0x${string}`>  
 
 }
 
-function verify(leaf: `0x${string}`, proofArray: Array<`0x${string}`>, root: `0x${string}`): boolean {
+export function verify(leaf: `0x${string}`, proofArray: Array<`0x${string}`>, root: `0x${string}`): boolean {
+    //For one address, if there is one address only in tree, then leaf and root will be same
+    if(leaf === root)
+        return true 
+
     let calculateRoot = leaf
     proofArray.forEach((proofArrElement) => {
         calculateRoot = calculatePairHash(proofArrElement, calculateRoot)
@@ -106,41 +110,28 @@ function verify(leaf: `0x${string}`, proofArray: Array<`0x${string}`>, root: `0x
 
 const addresses: Array<`0x${string}`> = [
   '0x1a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d7e8f9a0b',
-  '0x2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d7e8f9a0b1c',
-  '0x3c4d5e6f7a8b9c0d1e2f3a4b5c6d7e8f9a0b1c2d',
-  '0x4d5e6f7a8b9c0d1e2f3a4b5c6d7e8f9a0b1c2d3e',
-  '0x5e6f7a8b9c0d1e2f3a4b5c6d7e8f9a0b1c2d3e4f',
-  '0x6f7a8b9c0d1e2f3a4b5c6d7e8f9a0b1c2d3e4f5a',
-  '0x7a8b9c0d1e2f3a4b5c6d7e8f9a0b1c2d3e4f5a6b',
-  '0x8b9c0d1e2f3a4b5c6d7e8f9a0b1c2d3e4f5a6b7c',
-  '0x9c0d1e2f3a4b5c6d7e8f9a0b1c2d3e4f5a6b7c8d',
-  '0x0d1e2f3a4b5c6d7e8f9a0b1c2d3e4f5a6b7c8d9e',
-  '0x1e2f3a4b5c6d7e8f9a0b1c2d3e4f5a6b7c8d9e0f',
-  '0x2f3a4b5c6d7e8f9a0b1c2d3e4f5a6b7c8d9e0f1a',
-//   '0x3a4b5c6d7e8f9a0b1c2d3e4f5a6b7c8d9e0f1a2b',
-//   '0x4b5c6d7e8f9a0b1c2d3e4f5a6b7c8d9e0f1a2b3c',
-//   '0x5c6d7e8f9a0b1c2d3e4f5a6b7c8d9e0f1a2b3c4d',
-//   '0x6d7e8f9a0b1c2d3e4f5a6b7c8d9e0f1a2b3c4d5e'
+//   '0x2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d7e8f9a0b1c',
+//   '0x3c4d5e6f7a8b9c0d1e2f3a4b5c6d7e8f9a0b1c2d',
+//   '0x4d5e6f7a8b9c0d1e2f3a4b5c6d7e8f9a0b1c2d3e',
+//   '0x5e6f7a8b9c0d1e2f3a4b5c6d7e8f9a0b1c2d3e4f',
+//   '0x6f7a8b9c0d1e2f3a4b5c6d7e8f9a0b1c2d3e4f5a',
+//   '0x7a8b9c0d1e2f3a4b5c6d7e8f9a0b1c2d3e4f5a6b',
+//   '0x8b9c0d1e2f3a4b5c6d7e8f9a0b1c2d3e4f5a6b7c',
+//   '0x9c0d1e2f3a4b5c6d7e8f9a0b1c2d3e4f5a6b7c8d',
+//   '0x0d1e2f3a4b5c6d7e8f9a0b1c2d3e4f5a6b7c8d9e',
+//   '0x1e2f3a4b5c6d7e8f9a0b1c2d3e4f5a6b7c8d9e0f',
+//   '0x2f3a4b5c6d7e8f9a0b1c2d3e4f5a6b7c8d9e0f1a',
 ]
 
+// can be sorted too according to openZepplin merkle library
 // const sorted = [...arrayOfAddresses].sort((a,b) => Buffer.from(a.slice(2), 'hex').compare(Buffer.from(b.slice(2), 'hex')))
 
 const tree = createTree(addresses)
 
 console.log('Tree: ', tree)
 
-const proof = generateProof(tree, 9)
-console.log('Uncleaned Proof: ', proof)
-const cleanedProofArray = proof.filter((item) => item !== undefined)
-console.log('Cleaned Proof Array :',cleanedProofArray)
+const proof = generateProof(tree, 0)
+console.log('Proof: ', proof)
 
 //@ts-ignore
-console.log('Exists: ', verify(calculateAddressHash(addresses[9] as `0x${string}`), cleanedProofArray, tree[tree.length-1][0]))
-//TODO: Create an array by removing all undefined values from proof array.
-
-// console.log('Proof: ', proof)
-
-//for proof:
-// index - 0,2,4 -> index + 1, except if index =0 and index+1 does not exist, //TODO: then we will see what to do
-//index 1,3,5 -> index-1
-// we will be collecting one sibling per level and then adding them in proof array
+console.log('Exists: ', verify(calculateAddressHash(addresses[0] as `0x${string}`), proof, tree[tree.length-1][0]))
---
+++ src/App.tsx
@@ -0,0 +1,162 @@
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { useState } from "react";
+import {
+  createTree,
+  generateProof,
+  verify,
+  calculateAddressHash,
+} from "../merkle-tree/merkle-tree";
+import { ethers } from "ethers";
+import { toast } from "sonner";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+
+function App() {
+  const [addresses, setAddresses] = useState<string>("");
+  const [merkleRoot, setMerkleRoot] = useState<string>("");
+  const [ethAddresses, setEthAddresses] = useState<Array<string>>();
+  const [tree, setTree] = useState<Array<Array<`0x${string}`>>>();
+  const [address, setAddress] = useState<string>();
+  const [textProof, setTextProof] = useState<string>();
+  const [addressToVerify, setAddressToVerify] = useState<string>();
+  const [allProofs, setAllProofs] = useState<Array<Array<`0x${string}`>>>([[]]);
+
+  const arrayOfAddresses = addresses
+    .split("\n")
+    .map((line) => line.replace(/[\s.]/g, ""))
+    .filter((line) => line.length > 0);
+
+  const calculateMerkleRoot = () => {
+    for (const address of arrayOfAddresses) {
+      try {
+        checkValidAddresses(address as `0x${string}`);
+      } catch (error) {
+        toast.error("Invalid Ethereum Wallet Address");
+        // setMerkleRoot("");
+        return;
+      }
+    }
+    const tree = createTree(arrayOfAddresses as Array<`0x${string}`>);
+    setTree(tree);
+    setMerkleRoot(tree[tree.length - 1][0]);
+    setEthAddresses(arrayOfAddresses);
+
+    setAllProofs(
+      arrayOfAddresses.map((_, index) => generateProof(tree, index))
+    );
+  };
+
+  const checkValidAddresses = (address: `0x${string}`) => {
+    ethers.getAddress(address);
+  };
+
+  const calculateProof = () => {
+    try {
+      checkValidAddresses(address as `0x${string}`);
+    } catch (error) {
+      toast.error("Invalid Ethereum Wallet Address");
+      return;
+    }
+
+    const index = ethAddresses?.indexOf(address ?? "");
+    if (index !== -1)
+      setTextProof(JSON.stringify(generateProof(tree ?? [], index ?? 0)));
+    else
+      toast.error('Address does not exist in Merkle Tree')
+  };
+
+  const verifyAddress = () => {
+    try {
+      checkValidAddresses(addressToVerify as `0x${string}`);
+    } catch (error) {
+      toast.error("Invalid Ethereum Wallet Address");
+      return;
+    }
+
+    const proof = allProofs[arrayOfAddresses.indexOf(addressToVerify ?? "")];
+
+    console.log("Proof: ", proof);
+    if (proof === undefined) {
+      toast.error("Address does not exist in merkle tree");
+      return;
+    }
+
+    verify(
+      calculateAddressHash(addressToVerify as `0x${string}`),
+      proof,
+      merkleRoot as `0x${string}`
+    );
+
+    // if(exists)
+    toast.success("Address exists in Merkle Tree");
+    // else
+    // toast.error("Address does not exists")
+  };
+
+  return (
+    <>
+      <div className="flex flex-col items-center">
+        <div className="flex justify-center items-center w-full relative p-7">
+          <h1 className="font-bold text-4xl">Merkle Tree Generator</h1>
+          <p className="absolute right-0 p-5">
+            Created by{" "}
+            <a
+              href="https://github.com/Monis-Azeem"
+              target="_blank"
+              className="text-blue-700 font-bold underline"
+            >
+              Monis
+            </a>
+          </p>
+        </div>
+
+        {/* Generate Merkle Root */}
+        <div className="grid w-[50%] gap-4 p-5">
+          <Textarea
+            placeholder="Type your ethereum addresses here(One per Line)"
+            value={addresses}
+            onChange={(e) => setAddresses(e.target.value)}
+            cols={1}
+          />
+          <Button onClick={calculateMerkleRoot} disabled={addresses === ""}>
+            Generate Merkle Root
+          </Button>
+
+          <h2>{merkleRoot.length > 0 ? `Merkle Root: ${merkleRoot}` : ""}</h2>
+        </div>
+
+        {/* Generate Merkle Proof */}
+        <div className="gap-2 grid w-[50%] p-5">
+          <Label htmlFor="address">Generate Merkle Proof</Label>
+          <Input
+            type="text"
+            id="address"
+            placeholder="Enter Address"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+          <Button onClick={calculateProof}>Generate Proof</Button>
+          <Textarea placeholder="Proofs" disabled value={textProof} />
+        </div>
+
+        {/* Verification */}
+        <div className="gap-2 grid w-[50%] p-5">
+          <Label htmlFor="address">
+            Verify if an address belongs to Merkle Tree
+          </Label>
+          <Input
+            type="text"
+            id="address"
+            placeholder="Enter Address"
+            value={addressToVerify}
+            onChange={(e) => setAddressToVerify(e.target.value)}
+          />
+          <Button onClick={verifyAddress}>Verify Address</Button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
